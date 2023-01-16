57ac951151e6a31140777ef447d6fd91be95ffb8
kiranudhan <udhankiran62@gmail.com>
2023-01-16 14:57:22 +0530

    UC1->Calculate fare

--- src/main/java/InvoiceService.java
+++ src/main/java/InvoiceService.java
@@ -1,2 +1,8 @@
 public class InvoiceService {
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+    }
 }
